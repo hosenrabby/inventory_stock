e506d56e3d48a1577f8e26a08eba66d8b1b3461e
coderyousuf <yousuf2mail@gmail.com>
2022-08-30 00:41:51 -0700

    category crud updated

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\category;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\categoryRequest;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category=category::all();
+        return view('admin.category.index', compact('category'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(categoryRequest $request)
+    {
+        $input=$request->all();
+        category::create($input);
+        return redirect('authorized/category');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category=category::find($id);
+        return view('admin.category.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(categoryRequest $request, $id)
+    {
+        $category=category::find($id);
+        $input=$request->all();
+        $category->update($input);
+        return redirect('authorized/category');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category=category::find($id)->delete();
+        return back();
+    }
+}
