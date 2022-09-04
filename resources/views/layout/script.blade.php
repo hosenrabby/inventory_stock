ce88f5013ce34742834c119f69284bc225a334eb
hosenrabby <tahmid.tuhin.3@gmail.com>
2022-09-04 04:49:32 -0700

    Purchase form update

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -1,18 +1,4 @@
 
-<script src="{{ asset('public/assets/js/lib/calendar-2/moment.latest.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/calendar-2/pignose.calendar.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/calendar-2/pignose.init.js') }}"></script>
-
-
- <script src="{{ asset('public/assets/js/lib/weather/jquery.simpleWeather.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/weather/weather-init.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/circle-progress/circle-progress.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/circle-progress/circle-progress-init.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/chartist/chartist.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/sparklinechart/jquery.sparkline.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/sparklinechart/sparkline.init.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/owl-carousel/owl.carousel.min.js') }}"></script>
- <script src="{{ asset('public/assets/js/lib/owl-carousel/owl.carousel-init.js') }}"></script>
  <!-- scripit init-->
  <script src="{{ asset('public/assets/js/dashboard2.js') }}"></script>
  <script src="{{ asset('assets/js/lib/data-table/datatables.min.js') }}"></script>
@@ -34,6 +20,24 @@
 
  <script src="{{ asset('public/assets/js/lib/bootstrap.min.js') }}"></script>
  <script src="{{ asset('public/assets/js/scripts.js') }}"></script>
+
+<script src="{{ asset('public/assets/js/lib/calendar-2/moment.latest.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/calendar-2/pignose.calendar.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/calendar-2/pignose.init.js') }}"></script>
+
+
+ <script src="{{ asset('public/assets/js/lib/weather/jquery.simpleWeather.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/weather/weather-init.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/circle-progress/circle-progress.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/circle-progress/circle-progress-init.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/chartist/chartist.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/sparklinechart/jquery.sparkline.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/sparklinechart/sparkline.init.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/owl-carousel/owl.carousel.min.js') }}"></script>
+ <script src="{{ asset('public/assets/js/lib/owl-carousel/owl.carousel-init.js') }}"></script>
+
+
+
  <!-- bootstrap -->
  <script type="text/javascript">
     //==========================================
