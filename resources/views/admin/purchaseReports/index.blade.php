e2527dcb3cf904a6d320df9b40f2946657b3787e
hasanulbannadigitalmarketers <hasanalbanna2000@gmail.com>
2022-09-06 15:46:10 +0600

    sales Report Update

--- resources/views/admin/purchaseReports/index.blade.php
+++ resources/views/admin/purchaseReports/index.blade.php
@@ -8,7 +8,7 @@
                     <div class="col-lg-12 p-l-0 title-margin-left">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Table-Export</li>
+                            <li class="breadcrumb-item active">Purchase Repor</li>
                         </ol>
                     </div>
                     <!-- /# column -->
