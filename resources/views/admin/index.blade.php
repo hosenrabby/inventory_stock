aea4bbb312d96907617eb714d4f421f35c2cf3b0
hosenrabby <tahmid.tuhin.3@gmail.com>
2022-09-12 00:34:12 -0700

    for pull

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -101,28 +101,6 @@
                         </div>
                     </div>
                 </div>
-                {{-- <div class="row">
-                    <div class="col-lg-8">
-                        <div class="card">
-                            <div class="card-title">
-                                <h4>Fee Collections and Expenses</h4>
-
-                            </div>
-                            <div class="card-body">
-                                <div class="ct-bar-chart m-t-30"></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4">
-                        <div class="card">
-
-                            <div class="card-body">
-                                <div class="ct-pie-chart"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div> --}}
                 <div class="row">
                     <div class="col-lg-4">
                         <div class="row">
