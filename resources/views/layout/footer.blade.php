3e109aadd41ce252578b2c0559d8bc91ac84adb0
hosenrabby <tahmid.tuhin.3@gmail.com>
2022-09-19 02:05:28 -0700

    footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="footer">
-                        <p>2018 © Admin Board. - <a href="#">example.com</a></p>
+                        <p>2022 © Admin Board. - <a href="#">Inventory&stock</a></p>
                     </div>
                 </div>
             </div>
